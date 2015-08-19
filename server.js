1f4ed8e604b47faa5800ca0a1424cb95f5690dd5
giner7415 <giner7415@gmail.com>
2015-08-19 16:21:41 -0500

    El archivo ha subir solo puede ser de tipo .mp3

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-	var express =require("express"),
-	app=express(),
- 	bodyParser = require('body-parser'); 
+	var express =require("express");
 	var multer = require("multer");
-	//server=require('http').createServer(app),
+	bodyParser = require('body-parser'); 
 	path = require("path");
+	var app=express();
+	//var app = module.exports = express();
+	//server=require('http').createServer(app);
 	app.set('view engine','jade');
 	app.use(express.static('./public'));
 	
+ 	
+	//var port = process.env.PORT || 3000;
+
 	//Funciones importantes para subir archivos
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({extended:false}));
-	//var upload = multer({ dest: './public/uploads' })
+
 		
 	app.get('/',function  (req, res) {
 		res.render("index");
+		//res.send('Hello World');
 	});
 
 	var storage = multer.diskStorage({
@@ -25,32 +30,36 @@
 	  }
 	});
 
-	var upload = multer({ storage: storage })
+	var limits = { fileSize: 1024 * 1024 *1024};
 
-	//var fs=require('fs');//requerir modulo del sistema para escribir Archivos 
-	app.post('/subir',upload.single("miarchivo"),function (req,res) {
+	var fileFilter = function(req, file, cb) {
 		
-	
-		/*var tmp_path=req.body.miarchivo.path;//ruta del archivo
-		var tipo=req.body.miarchivo.type;//tipo del archivo
+	    if (path.extname(file.originalname) !== '.mp3') {
+	      	return cb(null, false);
+
+	    }
+	    else{
+	    	cb(null, true)
+	    }	
+	  };
+  
+
+	var upload = multer({ storage: storage,
+						limits: limits,
+						fileFilter: fileFilter
+
+						 });
+
+	app.post('/subir', upload.single("miArchivo"),function (req,res) {
 		
-		if(tipo=='image/png' || tipo=='image/jpg' || tipo=='image/jpeg' ){
+		  if (req.uploadError) {
+		    return res.end('Error uploading your new avatar')
+		  }
 
-			var nombrearchivo=req.body.miarchivo.name;//nombre del archivo 
+		  res.end('You new avatar is uploaded')
 
-			var target_path='./public/uploads/'+nombrearchivo;// hacia donde subiremos nuestro archivo dentro de nuestro servidor
-			fs.rename(tmp_path,target_path,function (err) {//Escribimos el archivo
-				//fs.unlink(tmp_path,function (err) {//borramos el archivo tmp
-					//damos una respuesta al cliente
-					res.send('<p>El archivo se subio correctamente</p></br><img  src="./uploads/'+nombrearchivo+'"/>');
-				//});
-			});
-
-		}else{
-			res.send('Tipo de archivo no soportado');
-		}
-*/
 	});
 
-
 	app.listen(3000);
+
+	
